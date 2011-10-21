0af037b71c3544caf57b4f2aefa45546fe763d88
Kira <kira@localhost.localdomain>
2011-10-21 20:21:50 +0800

    Update addBall and addCube

--- Structure.cpp
+++ Structure.cpp
@@ -27,11 +27,33 @@ bool Structure::operator==(const Structure& other) const
 
 int Structure::addBall(int px, int py, int pz, int radius)
 {
-
+  for (int i = (x-radius); i <= (x+radius); i++)
+  {
+    for(int j = (y-radius); j <= (y+radius); j++)
+    {
+      for(int k = (z-radius); k <= (z+radius); k++)
+      {
+	//Ball function
+	if ( pow(i-x,2)+pow(j-y,2)+pow(k-z,2) <= pow(radius, 2) )
+	  Array[i][j][k].permitivity = 11.7;
+      }
+    }
+  }
+    return 0;
 }
 
 int Structure::addCube(int px, int py, int pz, int radius)
 {
-
+  for (int i = (x-radius); i <= (x+radius); i++)
+  {
+    for(int j = (y-radius); j <= (y+radius); j++)
+    {
+      for(int k = (z-radius); k <= (z+radius); k++)
+      {
+	Array[i][j][k].permitivity = 11.7;
+      }
+    }
+  }
+    return 0;
 }
 
